ba378bd0347453e8bd118a700f42b10778c14ec2
Danielle Sucher <dsucher@gmail.com>
2013-10-30 20:30:16 -0400

    Add note on string:to_float(String ++ ".0")
    and clean up formatting, yeeeaaaah.

--- ask_area.erl
+++ ask_area.erl
@@ -11,9 +11,9 @@
 
 area() ->
   {ok, [ShapeChar]} = io:fread("R)ectangle, T)riangle, or E)llipse > ", "~s"),
-  {A, B} = get_dimensions("Enter width > ", "Enter height > "),
+  {Width, Height} = get_dimensions("Enter width > ", "Enter height > "),
   Shape = char_to_shape(ShapeChar),
-  calculate(Shape, A, B).
+  calculate(Shape, Width, Height).
 
 
 % private
@@ -21,10 +21,14 @@ area() ->
 -spec(calculate(atom(), number(), number()) -> number()).
 
 calculate(unknown, _, _) -> "Unknown shape.";
-calculate(_, A, _) when not is_number(A) -> "Error in first number.";
-calculate(_, _, B) when not is_number(B) -> "Error in second number.";
-calculate(_, A, B) when A < 0; B < 0 -> "Both numbers must be greater than or equal to zero.";
-calculate(Shape, A, B) -> geom:area(Shape, A, B).
+calculate(_, Width, _) when not is_number(Width) ->
+  "Error in first number.";
+calculate(_, _, Height) when not is_number(Height) ->
+  "Error in second number.";
+calculate(_, Width, Height) when Width < 0; Height < 0 ->
+  "Both numbers must be greater than or equal to zero.";
+calculate(Shape, Width, Height) ->
+  geom:area(Shape, Width, Height).
 
 
 -spec(char_to_shape(string()) -> atom()).
@@ -40,7 +44,8 @@ char_to_shape(Char) ->
 
 -spec(get_dimensions(string(), string()) -> {number(), number()}).
 
-get_dimensions(A_Prompt, B_Prompt) -> {get_number(A_Prompt), get_number(B_Prompt)}.
+get_dimensions(WidthPrompt, HeightPrompt) ->
+  {get_number(WidthPrompt), get_number(HeightPrompt)}.
 
 
 -spec(get_number(string()) -> number()).
@@ -52,3 +57,9 @@ get_number(Prompt) ->
     _                -> string:to_float(String)
   end,
   Number.
+
+% Also works:
+% get_number(Prompt) ->
+  % {ok, [String]} = io:fread(Prompt, "~s"),
+  % {Number, _} = string:to_float(String ++ ".0"),
+  % Number.
